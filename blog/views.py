0d904bf177571eb52c903998121784db82ba119e
Tejaswita Wakhure <31151862+TejaswitaW@users.noreply.github.com>
2023-07-25 19:03:23 +0530

    login template and views function done

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponseRedirect
 from .forms import UserSignUpForm,UserLoginForm
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 
 # Create your views here.
@@ -27,8 +28,24 @@ def sign_up(request):
     return render(request,'blog/signup.html',{'form':form})
 
 def user_login(request):
-    form = UserLoginForm()
-    return render(request,'blog/login.html',{'form':form})
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserLoginForm(request=request,data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                pwd = form.cleaned_data['password']
+                user = authenticate(username=uname,password=pwd)
+                if user != None:
+                    login(request,user)
+                    messages.success(request,'Logged in Successfully!')
+                    return HttpResponseRedirect('/dashboard/')
+
+        else:
+            form = UserLoginForm()
+        return render(request,'blog/login.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
 
 def user_logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
