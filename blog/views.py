1ef388b8f42f22dbda3977b549d68a7a94191860
Tejaswita Wakhure <31151862+TejaswitaW@users.noreply.github.com>
2023-07-26 11:48:48 +0530

    update view and template updated

--- blog/views.py
+++ blog/views.py
@@ -75,7 +75,14 @@ def add_post(request):
 def update_post(request,id):
     if request.user.is_authenticated:
         if request.method == "POST":
-            return render(request,'blog/updatepost.html')
+            pi = Post.objects.get(pk=id)
+            form = PostForm(request.POST,instance=pi)
+            if form.is_valid():
+                form.save()
+        else:
+            pi = Post.objects.get(pk=id)
+            form = PostForm(instance=pi)
+        return render(request,'blog/updatepost.html',{'form':form})
     else:
         return HttpResponseRedirect('/login/')
     
